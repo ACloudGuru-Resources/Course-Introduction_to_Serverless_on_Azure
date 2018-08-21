85d6cbc9a32bf83304768ae0faaf0f4159e2de4b
deanobalino <deanbryen@hotmail.com>
2018-08-22 00:17:18 +0100

    finished off createSpeaker function

--- createSpeaker/index.js
+++ createSpeaker/index.js
@@ -1,8 +1,26 @@
+var mongo = require('mongodb').MongoClient;
+
 module.exports = function (context, req) {
     context.log('createSpeaker function processing request');
-    if (req.body && req.body.name) {
-        let speakerData = req.body
-        context.bindings.speaker = JSON.stringify(speakerData);
+    context.log("req.body", req.body)
+    if (req.body) {
+        let speakerData = req.body;
+        
+        mongo.connect(process.env.speakers_COSMOSDB, (err, client) => {
+            context.log(err)
+            context.log(client)
+            let send = response(client, context);       
+            if (err) send(500, err.message);     
+            let db = client.db('acloudguru');      
+            db.collection('speakers').insertOne(
+              speakerData,
+              (err, speakerData) => {
+                if (err) send(500, err.message);
+        
+                send(200, speakerData);
+              }
+            );
+          });
     }
     else {
        context.res = {
@@ -10,5 +28,16 @@ module.exports = function (context, req) {
             body: "Please pass name in the body"
         }; 
     }
-    context.done();
+}
+
+function response(client, context) {
+    return function(status, body) {
+      context.res = {
+        status: status,
+        body: body
+      };
+  
+      client.close();
+      context.done();
+    };
 };
