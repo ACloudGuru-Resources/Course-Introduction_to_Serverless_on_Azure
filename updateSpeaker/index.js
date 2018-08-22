22413d92f230c8062023e5501ace26518abd8053
deanobalino <deanbryen@hotmail.com>
2018-08-22 01:05:33 +0100

    added updateSpeaker function

---
+++ updateSpeaker/index.js
@@ -0,0 +1,45 @@
+var mongo = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;  
+
+module.exports = function (context, req) {
+    context.log('createSpeaker function processing request');
+    context.log("req.body", req.body)
+    if (req.body) {
+        let speakerData = req.body;
+        
+        mongo.connect(process.env.speakers_COSMOSDB, (err, client) => {
+            let send = response(client, context);       
+            if (err) send(500, err.message);     
+            let db = client.db('acloudguru');    
+            
+            speakerData.id = req.query.id
+            db.collection('speakers').updateOne(
+                {_id: new ObjectID(speakerData.id) },
+                { $set: {name: speakerData.name, title : speakerData.title, location : speakerData.location, skills : speakerData.skills }},
+              (err, speakerData) => {
+                if (err) send(500, err.message);
+        
+                send(200, speakerData);
+              }
+            );
+          });
+    }
+    else {
+       context.res = {
+            status: 400,
+            body: "Please pass name in the body"
+        }; 
+    }
+}
+
+function response(client, context) {
+    return function(status, body) {
+      context.res = {
+        status: status,
+        body: body
+      };
+  
+      client.close();
+      context.done();
+    };
+};
